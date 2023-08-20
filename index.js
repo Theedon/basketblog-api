4435532c7f8252f42bcb2000b3cc890cd5602c83
Theedon <faniyantoyin@gmail.com>
2023-08-20 23:05:10 +0100

    change json database location for easy vercel access

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 const fs = require("fs");
-const jsonDataPath = "./data/dbase.json";
+const jsonDataPath = path.join(process.cwd(), "data/dbase.json");
 
 const PORT = 3000;
 const readJson = require("./readJson");
